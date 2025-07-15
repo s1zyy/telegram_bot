4e16560710a522906d0088cfef0795fb8eaf806c
Vlad <sav320801@gmail.com>
2025-07-15 20:59:08 +0100

    main menu

--- src/main/java/selfprojects/my_telegram_bot/UpdateConsumer.java
+++ src/main/java/selfprojects/my_telegram_bot/UpdateConsumer.java
@@ -1,17 +1,18 @@
 package selfprojects.my_telegram_bot;
 
-import lombok.SneakyThrows;
-import org.checkerframework.checker.units.qual.C;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import java.util.List;
 
 @Component
 public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
@@ -24,18 +25,70 @@ public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
 
     @Override
     public void consume(Update update) {
-        var chatId = update.getMessage().getChatId();
+
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            var chatId = update.getMessage().getChatId();
+
+            if(messageText.equals("/start")){
+                sendMainMenu(chatId);
+            }
+            else{
+                SendMessage sendMessage = SendMessage
+                        .builder()
+                        .text("I don't understand that command!")
+                        .chatId(chatId)
+                        .build();
+                try{
+                    telegramClient.execute(sendMessage);
+                }
+                catch (TelegramApiException e){
+                    System.out.println(e.getMessage());
+                }
+
+            }
+        }
+    }
+    private void sendMainMenu(Long chatId) {
         SendMessage sendMessage = SendMessage
                 .builder()
-                .text(update.getMessage().getText())
+                .text("Welcome, this is your main menu!")
                 .chatId(chatId)
                 .build();
-        try{
+
+
+        var button1 = InlineKeyboardButton
+                .builder()
+                .text("My name")
+                .callbackData("name")
+                .build();
+        var button2 = InlineKeyboardButton
+                .builder()
+                .text("Random picture")
+                .callbackData("long_process")
+                .build();
+        var button3 = InlineKeyboardButton
+                .builder()
+                .text("Random number")
+                .callbackData("number")
+                .build();
+
+
+        List<InlineKeyboardRow> buttons = List.of(
+                new InlineKeyboardRow(button1),
+                new InlineKeyboardRow(button2),
+                new InlineKeyboardRow(button3)
+        );
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup(buttons);
+
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        try {
             telegramClient.execute(sendMessage);
-        }
-        catch (TelegramApiException e){
+        } catch (TelegramApiException e) {
             System.out.println(e.getMessage());
         }
-        System.out.printf("Update received from %s, message is: %s%n", update.getMessage().getText(), update.getMessage().getChatId());
+
+
+
     }
 }
